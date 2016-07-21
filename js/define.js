6ae3dbfa63d820b39e33390f2057d4e50394b0c9
Andnopomc <andnopo@gmail.com>
2016-07-21 08:58:48 -0400

    Git

---
+++ js/define.js
@@ -0,0 +1,4 @@
+function cd(on) {
+	loc = on.replace("cd ", "");
+	window.location.href = loc;
+}
---
+++ js/function.js
@@ -0,0 +1,14 @@
+function ie(text) {
+	if (text.charAt(0) + text.charAt(1) == "cd") {
+		cd(text);
+	}
+	else if (text == "man") {
+		man();
+	}
+	else if (text == "help") {
+	
+	}
+	else if (text == "cm") {
+	
+	}
+}
